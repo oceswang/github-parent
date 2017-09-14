9df4ff127def77ef521448f78fc6c362d9624b80
Wang Song <songwang@galaxyintrnet.com>
2017-09-14 15:55:10 +0800

    new ui

--- github-sso/github-sso-core/src/main/java/com/github/sso/core/utils/SSOConstants.java
+++ github-sso/github-sso-core/src/main/java/com/github/sso/core/utils/SSOConstants.java
@@ -4,6 +4,7 @@ public class SSOConstants
 {
 	public static final String CACHE_CODE_PREFIX = "sso:code:";
 	public static final String CACHE_TOKEN_PREFIX = "sso:token:";
+	public static final String CACHE_EXPIRE_PREFIX = "expire:";
 	
 	public static final String SESSION_USER_LOGIN = "user_login";
 }
--- github-sso/github-sso-core/src/main/java/com/github/sso/core/web/MainController.java
+++ github-sso/github-sso-core/src/main/java/com/github/sso/core/web/MainController.java
@@ -80,7 +80,7 @@ public class MainController
 			{
 				if(!checkLogin(request,response))
 				{
-					return "/page-login";
+					return "/login_bg";
 				}
 			}
 			user = (UserDTO)request.getSession(true).getAttribute(SSOConstants.SESSION_USER_LOGIN);
